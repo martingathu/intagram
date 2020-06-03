b1ac1850b72e101bc2b4df7851ebbca9969e95f7
martingathu <martin5gathu@gmail.com>
2020-06-03 13:46:57 +0300

    add model class for post

--- photos/models.py
+++ photos/models.py
@@ -5,23 +5,23 @@ import django.utils.timezone
 
 
 
-# # Create your models here.
-# class Post(models.Model):
-#     caption = models.CharField(max_length=60)
-#     user = models.ForeignKey(User, on_delete=models.CASCADE)
-#     pub_date = models.DateTimeField(auto_now_add=True)
-#     post_image = models.ImageField(upload_to = 'posts/')
+# Create your models here.
+class Post(models.Model):
+    caption = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    post_image = models.ImageField(upload_to = 'posts/')
 
-#     @classmethod
-#     def get_posts(cls):
-#         posts = cls.objects.all()
-#         return posts
+    @classmethod
+    def get_posts(cls):
+        posts = cls.objects.all()
+        return posts
 
-#     def save_post(self):
-#         self.save()
+    def save_post(self):
+        self.save()
 
-#     def delete_post(self):
-#         self.delete()
+    def delete_post(self):
+        self.delete()
 
 # class Comment(models.Model):
 #     post = models.ForeignKey(Post, on_delete=models.CASCADE,)
