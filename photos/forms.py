447671c493fe6dc964ab11f1934fea42fba29348
martingathu <martin5gathu@gmail.com>
2020-06-03 13:54:37 +0300

    add a form for signup

--- photos/forms.py
+++ photos/forms.py
@@ -4,18 +4,18 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
-# class SignUpForm(UserCreationForm):
-#     def __init__(self, *args, **kwargs):
-#         super(SignUpForm, self).__init__(*args, **kwargs)
+class SignUpForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super(SignUpForm, self).__init__(*args, **kwargs)
 
-#         for fieldname in ['username', 'password1', 'password2']:
-#             self.fields[fieldname].help_text = None
+        for fieldname in ['username', 'password1', 'password2']:
+            self.fields[fieldname].help_text = None
 
-#     email = forms.EmailField(max_length=254, required=True)
+    email = forms.EmailField(max_length=254, required=True)
 
-#     class Meta:
-#         model = User
-#         fields = ('email' ,'username','password1', 'password2', )
+    class Meta:
+        model = User
+        fields = ('email' ,'username','password1', 'password2', )
 
 # class NewPostForm(forms.ModelForm):
 #     class Meta:
