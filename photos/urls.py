c9df849c714d797f1b0b7ebc308249bd9b132c2c
martingathu <martin5gathu@gmail.com>
2020-06-03 13:43:26 +0300

    add media settings

--- photos/urls.py
+++ photos/urls.py
@@ -14,5 +14,5 @@ urlpatterns = [
     url(r'^updateprofile/', views.update_profile, name='update_profile'),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
