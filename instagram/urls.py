6d809081b8999be2ff4dce102874d9dbccc8c677
martingathu <martin5gathu@gmail.com>
2020-06-05 12:43:29 +0300

    add url for user profile

--- instagram/urls.py
+++ instagram/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url('', include('photos.urls')),
-    # url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^tinymce/', include('tinymce.urls')),
 ]
 
