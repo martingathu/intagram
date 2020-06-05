d83263ec939323b32e57c1af2886edd50002d76f
martingathu <martin5gathu@gmail.com>
2020-06-05 09:13:05 +0300

    add a url for the homepage

--- photos/urls.py
+++ photos/urls.py
@@ -5,13 +5,15 @@ from django.conf.urls.static import static
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
+    url('^$', views.index, name='home'),
     url(r'^signup', views.signup, name='signup'),
-    url(r'', views.index, name='home'),
     url(r'^login', LoginView.as_view(), name='login_url'),
     url(r'^logout/', LogoutView.as_view(next_page='login_url'), name='logout_url'),
-    url(r'^newpost/', views.new_post, name='new_post'),
-    url(r'^profile/', views.profile, name='profile'),
-    url(r'^updateprofile/', views.update_profile, name='update_profile'),
+    url(r'^newpost/$', views.new_post, name='new_post'),
+    url(r'^user/(\d+)$', views.profile, name='profile'),
+    url(r'^profile/edit/$', views.update_profile, name='update_profile'),
+    # url(r'^updateprofile/', views.update_profile, name='update_profile'),
+  
 ]
 
 if settings.DEBUG:
