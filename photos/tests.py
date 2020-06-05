dd94ca05c47dbfc622e0c949d3c09cc2395ed68d
martingathu <martin5gathu@gmail.com>
2020-06-05 13:50:03 +0300

    add test class for the posts

--- photos/tests.py
+++ photos/tests.py
@@ -6,3 +6,19 @@ from .models import Profile, Post, Comment
 from django.contrib.auth.models import User
 
 
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.post = Post(caption = 'mynewshoe',)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.post, Post))
+
+    def test_save_post(self):
+        self.post.save_post()
+        post = Post.objects.all()
+        self.asserTrue(len(post)>0)
+
+    def test_delete_images(self):
+        self.post.delete_post()
+        post = Post.objects.all()
+        self.asserTrue(len(post) == 0)
