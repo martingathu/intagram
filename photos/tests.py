555eadc4649f4e8cdeb237ca29f4181350f373bd
martingathu <martin5gathu@gmail.com>
2020-06-07 10:53:56 +0300

    add test class for profile model class

--- photos/tests.py
+++ photos/tests.py
@@ -1,24 +1,72 @@
 from django.test import TestCase
-
-# Create your tests here.
-from django.test import TestCase
-from .models import Profile, Post, Comment
+from .models import Post,Profile,Comment
 from django.contrib.auth.models import User
 
+# Create your tests here.
 
-class PostTestClass(TestCase):
+class ProfileTestClass(TestCase):
+    '''
+    a class to test the profile instances and its methods
+    '''
+
+    # Set up method
     def setUp(self):
-        self.post = Post(caption = 'mynewshoe',)
-
+        self.user = User.objects.create(id=1,username='martin')
+        self.profile= Profile(profile_photo='/static/images/log.png',bio='dev',user=self.user)
+    
+    #Testing Instance
     def test_instance(self):
-        self.assertTrue(isinstance(self.post, Post))
+        self.assertTrue(isinstance(self.profile,Profile))
+        
+    #Testing the save method
+    '''
+    function to check the save method of profile
+    '''
+    
+    def test_save_method(self):
+        self.profile.save_profile()
+        profile = Profile.objects.all()
+        self.assertTrue(len(profile)>=1)
+        
+    #Testing the delete method
+    def test_delete_method(self):
+        '''
+        function to check the delete method of profile
+        '''
+        self.profile.save_profile()
+        profil= Profile.objects.filter(profile_photo='/static/images/log.png').first()
+        delete= Profile.objects.filter(profile_photo=profil.profile_photo).delete()
+        profile=Profile.objects.all()
+        self.assertFalse(len(profile)==1)
 
-    def test_save_post(self):
-        self.post.save_post()
-        post = Post.objects.all()
-        self.asserTrue(len(post)>0)
+    #Testing the update method
+    def test_update_profile(self):
+        '''
+        a method that helps to update the profile
+        
+        ''' 
+        self.profile.save_profile()
+        profil= Profile.objects.filter(profile_photo='/static/images/log.png').first()
+        update = Profile.objects.filter(profile_photo=profil.profile_photo).update(profile_photo='pic')
+        updated = Profile.objects.filter(profile_photo='pic').first()
+        self.assertNotEqual(profil.profile_photo,updated.profile_photo)
+        
+        
+class PostTestClass(TestCase):
+    '''
+    a class to test the image instances and its methods
+    '''
 
-    def test_delete_images(self):
-        self.post.delete_post()
-        post = Post.objects.all()
-        self.asserTrue(len(post) == 0)
+    # Set up method
+    def setUp(self):
+        self.user = User.objects.create(id=1,username='martin')
+        self.post_image= Post(post_image='/static/images/log.png',caption='lovely',pub_date='2pm',like='1',user=self.user)
+    
+ 
+    
+
+        
+ 
+   
+            
+            
