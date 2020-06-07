7124f021179558ff1725d441f9067596ea2ebfd7
martingathu <martin5gathu@gmail.com>
2020-06-07 09:23:17 +0300

    add a view function for like button

--- photos/views.py
+++ photos/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .forms import SignUpForm, NewPostForm, CommentForm, ProfileForm
 from django.core.mail import send_mail
-from .models import Post, Comment, Profile
+from .models import Post, Comment, Profile, Follow
 from django.contrib.auth.models import User
+from django.http import HttpResponse, Http404,HttpResponseRedirect
 
 @login_required(login_url='/login/')
 def index(request):
@@ -69,16 +70,19 @@ def update_profile(request):
     current_user = request.user
     profile = Profile.objects.get(user=request.user)
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
+        form = ProfileForm(request.POST, request.FILES, instance=Profile.objects.get(user_id=current_user))
         if form.is_valid():
-            profile = form.save(commit=False)
-            profile.save()
+                profile = form.save(commit=False)
+                profile.user = current_user
+                profile.save()
         return redirect('home')
+
+    
     else:
-        form = ProfileForm(instance=request.user.profile)
-        args = {}
-        # args.update(csrf(request))
-        args['form'] = form
+        if Profile.objects.filter(user_id=current_user).exists():
+            form = ProfileForm(instance = Profile.objects.get(user_id=current_user))
+        else:
+            form = ProfileForm()
     return render(request, 'profile/edit-profile.html', {'current_user':current_user, 'form':form})
   
 @login_required(login_url='/accounts/login/')
@@ -87,27 +91,52 @@ def profile(request, user_id):
     Function that enables one to see their profile
     """
     current_user = request.user
-
+    user = User.objects.get(pk=user_id)
     posts = Post.get_posts()
     comments = Comment.get_comments()
+    credentials = Profile.objects.filter(user = user_id)
     
-    return render(request, 'profile/profile.html', {'current_user':current_user, 'posts':posts, 'comments':comments})
+    if Follow.objects.filter(following=request.user,follower= user).exists():
+        follows_me =True
+
+    else:
+        follows_me=False
+    followers=Follow.objects.filter(follower = user).count()
+    following=Follow.objects.filter(following = user).count()
+
+    return render(request, 'profile/profile.html', {'follows_me':follows_me,'following':following,'followers':followers,'current_user':current_user, 'posts':posts, 'comments':comments, 'credentials':credentials})
 
 
-@login_required(login_url='/accounts/login/')
 def search_user(request):
     """
     Function that searches for profiles based on the usernames
     """
     if 'username' in request.GET and request.GET["username"]:
-        name = request.GET.get("username")
-        searched_profiles = User.objects.filter(username__icontains=name)
-        message = f"{name}"
+        search_term = request.GET.get("username")
+        searched_profiles = User.objects.filter(username__icontains=search_term)
+        message = f"{search_term}"
         profiles = User.objects.all()
-        # people = Follow.objects.following(request.user)
-        print(profiles)
+        
         return render(request, 'search.html', {"message": message, "usernames": searched_profiles, "profiles": profiles, })
 
     else:
-        message = "Enter search term"
+        message = "You haven't searched for any term"
         return render(request, 'search.html', {"message": message})
+
+def follow(request,user_id):
+    user = User.objects.get(id=user_id)
+    follows_me=False
+    if Follow.objects.filter(following=request.user,follower=user).exists():
+        Follow.objects.filter(following=request.user,follower=user).delete()
+        follows_me=False
+    else:
+        Follow(following=request.user,follower=user).save()
+        follows_me=True
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def likes(request,id):
+    likes=0
+    post = Post.objects.get(id=id)
+    post.like = post.like+1
+    post.save()    
+    return redirect("home")
